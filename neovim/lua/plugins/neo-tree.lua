c6bb9e76811b4d4c5b051d887ea359fc32b35311
Sergej Sanikovic <sergej.sanikovic@outlook.com>
2024-03-03 00:10:06 +0100

    made seperate keybindings for differnt views

--- neovim/lua/plugins/neo-tree.lua
+++ neovim/lua/plugins/neo-tree.lua
@@ -8,18 +8,40 @@ return {
     },
     window = {
     mappings = {
-      ['e'] = function() vim.api.nvim_exec('Neotree focus filesystem left', true) end,
+      ['t'] = function() vim.api.nvim_exec('Neotree focus filesystem left', true) end,
       ['b'] = function() vim.api.nvim_exec('Neotree focus buffers left', true) end,
       ['g'] = function() vim.api.nvim_exec('Neotree focus git_status left', true) end,
     },
     },
     keys = {
       {
-        "<A-r>",
+        "<A-t>",
         function()
           require("neo-tree.command").execute({
             toggle = true,
-            source = "last",
+            source = "filesystem",
+            position = "left",
+          })
+        end,
+        desc = "Filesystem (root dir)",
+      },
+      {
+        "<A-g>",
+        function()
+          require("neo-tree.command").execute({
+            toggle = true,
+            source = "git_status",
+            position = "left",
+          })
+        end,
+        desc = "Git Status (root dir)",
+      },
+      {
+        "<A-b>",
+        function()
+          require("neo-tree.command").execute({
+            toggle = true,
+            source = "buffers",
             position = "left",
           })
         end,
