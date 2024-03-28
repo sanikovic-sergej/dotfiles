76745dcc7fe7c55f02d47ac65a87bf605db64de4
Sergej Sanikovic <sergej.sanikovic@outlook.com>
2024-03-28 20:36:41 +0100

    added todo-comments plugin with suggested default settings

--- neovim/lua/keybindings.lua
+++ neovim/lua/keybindings.lua
@@ -62,3 +62,17 @@ which_key.register({
         t = { function() vim.api.nvim_exec('bd', true) end, "Close current Buffer" },
     },
 })
+-- TODO: integrate the suggested todo-comments keybinding into which-key
+vim.keymap.set("n", "]t", function()
+    require("todo-comments").jump_next()
+end, { desc = "Next todo comment" })
+
+vim.keymap.set("n", "[t", function()
+    require("todo-comments").jump_prev()
+end, { desc = "Previous todo comment" })
+
+-- You can also specify a list of valid jump keywords
+
+vim.keymap.set("n", "]t", function()
+    require("todo-comments").jump_next({ keywords = { "ERROR", "WARNING" } })
+end, { desc = "Next error/warning todo comment" })
