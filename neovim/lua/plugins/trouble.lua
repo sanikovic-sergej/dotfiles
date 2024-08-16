065a057788883928bf4639fed72f81981949af76
Sergej Sanikovic <ssanikovic@Sergejs-Monster.fritz.box>
2024-08-16 15:52:22 +0200

    fixed trouble plugin not working

--- neovim/lua/plugins/trouble.lua
+++ neovim/lua/plugins/trouble.lua
@@ -1,6 +1,7 @@
 return {
     "folke/trouble.nvim",
-    branch = "dev", -- IMPORTANT!
+    opts = {}, -- for default options, refer to the configuration section for custom setup.
+    cmd = "Trouble",
     keys = {
         {
             "<leader>xx",
