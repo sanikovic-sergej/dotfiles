09b5995b6be2e551fc0605525374df5b1e4091a8
Sergej Sanikovic <ssanikovic@Sergejs-Monster.fritz.box>
2024-08-16 16:27:33 +0200

    updated init of lazy plugin manager

--- neovim/init.lua
+++ neovim/init.lua
@@ -1,4 +1,7 @@
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
 vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
 
 require("packet-manager")
 require("vim-options")
