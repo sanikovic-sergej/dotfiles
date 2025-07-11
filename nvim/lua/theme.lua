f353482f9b38adeb791ce64c86fdafe63627030c
Sanikovic, Sergej <sergej.sanikovic@outlook.com>
2025-07-11 21:52:33 +0200

    improved symlink script
    improved symlink script

--- nvim/lua/theme.lua
+++ nvim/lua/theme.lua
@@ -1,4 +1,4 @@
-vim.cmd[[colorscheme tokyonight-storm]]
+vim.cmd [[colorscheme tokyonight-storm]]
 --vim.o.background = "light"
 --vim.api.nvim_set_hl(0, "Normal", {bg = "none" })
 --vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none" })
