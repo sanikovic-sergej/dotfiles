98f4edcb48235eb342ae044b953c9e4b05af7d51
Sanikovic, Sergej <sergej.sanikovic@outlook.com>
2025-11-15 20:47:52 +0100

    switched to docker api compatible watchtower

--- fish/conf.d/abbreviations.fish
+++ fish/conf.d/abbreviations.fish
@@ -31,7 +31,7 @@ abbr --add dotfiles "cd ~/.dotfiles"
 abbr --add lg lazygit
 abbr --add git_config_debug "git config --list --show-origin --show-scope"
 
-abbr --add sd-automatic "source ~/Projects/stable-diffusion/stable-diffusion-webui/venv/bin/activate && ~/Projects/stable-diffusion/stable-diffusion-webui/webui.sh"
-abbr --add sd-comfyui "source ~/Projects/stable-diffusion/ComfyUI/venv/bin/activate && HSA_OVERRIDE_GFX_VERSION=10.3.0 HIP_VISIBLE_DEVICES=0 PYTORCH_HIP_ALLOC_CONF=expandable_segments:True python ~/Projects/stable-diffusion/ComfyUI/main.py"
+abbr --add sd-automatic "source ~/Projects/stable-diffusion/stable-diffusion-webui/venv/bin/activate.fish && ~/Projects/stable-diffusion/stable-diffusion-webui/webui.sh"
+abbr --add sd-comfyui "source ~/Projects/stable-diffusion/ComfyUI/venv/bin/activate.fish && set -x HSA_OVERRIDE_GFX_VERSION 10.3.0 && set -x HIP_VISIBLE_DEVICES 0 && set -x PYTORCH_HIP_ALLOC_CONF expandable_segments:True && set -x PYTORCH_TUNABLEOP_ENABLED 1 && python ~/Projects/stable-diffusion/ComfyUI/main.py --use-pytorch-cross-attention"
 abbr --add setup_amd_export_llm_magic "export HSA_OVERRIDE_GFX_VERSION=10.3.0 export HIP_VISIBLE_DEVICES=0 export PYTORCH_HIP_ALLOC_CONF=garbage_collection_threshold:0.8,max_split_size_mb:512"
 abbr --add update-open-webui "docker run --rm -v /var/run/docker.sock:/var/run/docker.sock nickfedor/watchtower --run-once open-webui"
